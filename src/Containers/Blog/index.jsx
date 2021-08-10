5c4bfe62c30396e83164aa36f6fc3fada9d482a9
Adarsh Mallik <adarsh.mallik93@gmail.com>
2021-08-10 08:36:20 +0530

    Axios-change

--- src/Containers/Blog/index.jsx
+++ src/Containers/Blog/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Image from "../../img/samsungtv.jpg";
+import axios from "axios"
 
 class Blogs extends React.Component {
   constructor(props) {
@@ -29,12 +30,77 @@ class Blogs extends React.Component {
     };
   }
   componentDidMount() {
+    //API calls are done here
+
+    //Example for fetch api
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((data) => {
         this.setState({ ...this.state, users: data });
         console.log(this.state.users)
       });
+
+      //example for AXIOS
+
+      axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
+           console.log(res);
+      })
+
+      axios.post("https://jsonplaceholder.typicode.com/users",{
+        id: 10,
+        name: "Leanne Graham",
+        username: "Bret",
+        email: "Sincere@april.biz",
+        address: {
+          street: "Kulas Light",
+          suite: "Apt. 556",
+          city: "Gwenborough",
+          zipcode: "92998-3874",
+          geo: {
+            lat: "-37.3159",
+            lng: "81.1496"
+          }
+        },
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+          name: "Romaguera-Crona",
+          catchPhrase: "Multi-layered client-server neural-net",
+          bs: "harness real-time e-markets"
+        }
+      }).then((res) => {
+        console.log(res)
+      })
+
+      axios.put("https://jsonplaceholder.typicode.com/users/1",{
+        
+        name: "John Snow",
+        username: "Bret",
+        email: "JohnSnow@april.biz",
+        address: {
+          street: "Kulas Light",
+          suite: "Apt. 556",
+          city: "Gwenborough",
+          zipcode: "92998-3874",
+          geo: {
+            lat: "-37.3159",
+            lng: "81.1496"
+          }
+        },
+        phone: "1-770-736-8031 x56442",
+        website: "hildegard.org",
+        company: {
+          name: "Romaguera-Crona",
+          catchPhrase: "Multi-layered client-server neural-net",
+          bs: "harness real-time e-markets"
+        }
+      }).then((res)=>{
+        console.log(res)
+      })
+
+      axios.delete("https://jsonplaceholder.typicode.com/users/1").then((res) => {
+        console.log(res)
+      })
   }
   render() {
     let allBlogs = this.state.blogs.map((item, index) => {
